9da824593816cadef1e3d33a0aac8c70a26654c8
Lyudmil Delchev <lddelchev@gmail.com>
2017-08-09 20:11:35 +0300

    first commit by Lyudmil

--- shared.java
+++ shared.java
@@ -1,4 +1,8 @@
 public class GitExample {
+
+	public void greetingFromLyudmil() {
+		System.out.println("Hello by Lyudmil!!!");
+	}
     
     public void saySomething(String something_hahah) {
        System.out.println(something_hahah);
