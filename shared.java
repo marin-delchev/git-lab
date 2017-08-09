60739c9b4e8c251615840a03ede278e0da070b74
marin-delchev <MarinDelchev@protonmail.com>
2017-08-09 18:40:41 +0300

    Add java toy example

---
+++ shared.java
@@ -0,0 +1,16 @@
+public class GitExample {
+    
+    public void saySomething(String something) {
+       System.out.println(something);
+    }
+    
+    
+    public static void main(String args[]) {
+        
+        GitExample gitExample = new GitExample();
+        gitExample.saySomething("Git is cool");
+        
+    }
+    
+}
+
