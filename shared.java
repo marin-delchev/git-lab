b812764cc659e940ff5732d893dfee6f0ca83603
Pavel <pavel@localhost.localdomain>
2017-08-09 19:31:48 +0200

    Added new method to improve stability.

--- shared.java
+++ shared.java
@@ -3,12 +3,17 @@ public class GitExample {
     public void saySomething(String something) {
        System.out.println(something);
     }
+
+    public static void printSomething() {
+        System.out.println("Something!");
+    }
     
     
     public static void main(String args[]) {
         
         GitExample gitExample = new GitExample();
         gitExample.saySomething("Git is cool");
+        printSomething();
         
     }
     
